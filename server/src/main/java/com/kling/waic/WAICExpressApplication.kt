7f1a66144bdc8010bd3391e31c684b9c5b9a5ddb
haojiakang <haojiakang@kuaishou.com>
2025-07-08 06:52:21 +0000

    Rename main class

--- server/src/main/java/com/kling/waic/WAICExpressApplication.kt
+++ server/src/main/java/com/kling/waic/WAICExpressApplication.kt
@@ -4,13 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class Application {
+open class WAICExpressApplication {
 
     companion object {
 
         @JvmStatic
         fun main(args: Array<String>) {
-            runApplication<Application>(*args)
+            runApplication<WAICExpressApplication>(*args)
         }
     }
 }
