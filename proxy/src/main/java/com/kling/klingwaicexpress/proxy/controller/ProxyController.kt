e9c497e433cbecac815e1400f9721811180a8962
haojiakang <haojiakang@kuaishou.com>
2025-07-15 14:48:55 +0800

    Add proxy layer

---
+++ proxy/src/main/java/com/kling/klingwaicexpress/proxy/controller/ProxyController.kt
@@ -0,0 +1,51 @@
+package com.kling.klingwaicexpress.proxy.controller
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+
+@RestController
+class ProxyController(
+    @Value("\${kling-waic-server.host}")
+    private val host: String,
+    @Value("\${kling-waic-server.port}")
+    private val port: Int,
+    private val webClientBuilder: WebClient.Builder
+) {
+
+    private val targetBaseUrl = "http://$host:$port"
+
+    @RequestMapping("/**",
+        method = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH])
+    fun proxy(request: HttpServletRequest,
+              entity: HttpEntity<String>): ResponseEntity<Mono<String>> {
+        val method = HttpMethod.valueOf(request.method)
+        val path = request.requestURI.replace("/proxy", "/api")
+        val query = request.queryString?.let { "?$it" } ?: ""
+        val url = "$targetBaseUrl$path$query"
+
+        val headers = HttpHeaders()
+        request.headerNames.toList().forEach {
+            headers[it] = request.getHeaders(it).toList()
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+            webClientBuilder.build()
+                .method(method)
+                .uri(url)
+                .headers { it.putAll(headers) }
+                .bodyValue(entity.body ?: "")
+                .retrieve()
+                .bodyToMono(String::class.java)
+        )
+    }
+}
