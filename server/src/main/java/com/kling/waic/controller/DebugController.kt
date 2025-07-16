ee91ed3452df59e0dae1d423f6fa72cbc6c0e1f7
haojiakang <haojiakang@kuaishou.com>
2025-07-16 15:18:00 +0800

    Remove debug controller

--- server/src/main/java/com/kling/waic/controller/DebugController.kt
+++ server/src/main/java/com/kling/waic/controller/DebugController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.io.File
 
-@RestController
+//@RestController
 @RequestMapping("/debug")
 class DebugController(
     @Value("\${waic.sudoku.images-dir}")
