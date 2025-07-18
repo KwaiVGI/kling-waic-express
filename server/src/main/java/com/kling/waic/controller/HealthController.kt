c4689dd63b275de9b974a168fa26d855bb976010
haojiakang <haojiakang@kuaishou.com>
2025-07-18 20:12:15 +0800

    Correct health check

--- server/src/main/java/com/kling/waic/controller/HealthController.kt
+++ server/src/main/java/com/kling/waic/controller/HealthController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 
 @RestController
-@RequestMapping("/api/health")
+@RequestMapping("/health")
 class HealthController {
 
     @GetMapping("/heartbeat")
