e8d1dc8424ddedf82a425279362d345ff5501532
haojiakang <haojiakang@kuaishou.com>
2025-08-19 18:05:50 +0800

    Support AdminConfig

--- server/api/src/main/kotlin/com/kling/waic/api/controller/AdminConfigController.kt
+++ server/api/src/main/kotlin/com/kling/waic/api/controller/AdminConfigController.kt
@@ -2,7 +2,7 @@ package com.kling.waic.api.controller
 
 import com.kling.waic.component.auth.Authorization
 import com.kling.waic.component.auth.AuthorizationType
-import com.kling.waic.component.entity.AdminConfig
+import com.kling.waic.component.config.AdminConfig
 import com.kling.waic.component.entity.Result
 import com.kling.waic.component.helper.AdminConfigHelper
 import org.springframework.web.bind.annotation.*
