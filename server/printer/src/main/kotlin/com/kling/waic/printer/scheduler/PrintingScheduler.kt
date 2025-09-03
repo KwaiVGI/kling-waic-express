9d0f962d549245b56b0c726ff94a2917bdcc751c
haojiakang <haojiakang@kuaishou.com>
2025-09-03 16:41:36 +0800

    Add printAllConfigs

--- server/printer/src/main/kotlin/com/kling/waic/printer/scheduler/PrintingScheduler.kt
+++ server/printer/src/main/kotlin/com/kling/waic/printer/scheduler/PrintingScheduler.kt
@@ -2,6 +2,9 @@ package com.kling.waic.printer.scheduler
 
 import com.kling.waic.component.utils.Slf4j.Companion.log
 import com.kling.waic.printer.adapter.PrintAdapter
+import org.springframework.core.env.AbstractEnvironment
+import org.springframework.core.env.Environment
+import org.springframework.core.env.MapPropertySource
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.SchedulingConfigurer
 import org.springframework.scheduling.config.ScheduledTaskRegistrar
@@ -12,10 +15,14 @@ import java.util.TimeZone
 @Component
 @EnableScheduling
 class PrintingScheduler(
-    private val printAdapter: PrintAdapter
+    private val printAdapter: PrintAdapter,
+    private val env: Environment
 ) : SchedulingConfigurer {
 
     override fun configureTasks(taskRegistrar: ScheduledTaskRegistrar) {
+        log.info("Print All Configs:")
+        printAllConfigs()
+
         val cron = "*/1 * * * * *"
         val timezone = TimeZone.getTimeZone("Asia/Shanghai")
         log.info("Cron task registered, cron: {}, timezone: {}", cron, timezone)
@@ -32,5 +39,17 @@ class PrintingScheduler(
                 CronTrigger(cron, timezone).nextExecution(triggerContext)
             }
         )
+    }
+
+    fun printAllConfigs() {
+        val propertySources = (env as AbstractEnvironment).propertySources
+            .filterIsInstance<MapPropertySource>()
+
+        val allProperties = propertySources
+            .flatMap { it.source.entries }
+
+        allProperties.forEach { (key, value) ->
+            println("$key = $value")
+        }
     }
 }
